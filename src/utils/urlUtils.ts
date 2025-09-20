3c9149aa3e70aefa19e86c873058970129292af9
Blue292861 <neils.hamelin.28@gmail.com>
2025-09-20 20:45:14 +0200

    Update urlUtils.ts

--- src/utils/urlUtils.ts
+++ src/utils/urlUtils.ts
@@ -1,6 +1,7 @@
-
+// src/utils/urlUtils.ts
 export const isUrl = (str: string): boolean => {
   try {
+    // Vérifie si la chaîne de caractères est une URL valide
     new URL(str);
     return true;
   } catch (_) {
